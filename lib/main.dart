950ce1a66061335fc77228f97c19e20b058a1879
SohanZiyaei <sobhan.ziaee73@gmail.com>
2023-07-03 15:29:05 +0430

    create splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:splash_view/splash_view.dart';
+import 'package:travel_app/ui/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(
+        logo: const Text(
+          'Explore Your Favorite Journey',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        subtitle: const Text(
+          "Let's Make Our Life better",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        backgroundImageDecoration: const BackgroundImageDecoration(
+          image: AssetImage('assets/images/jungle.jpg'),
+          fit: BoxFit.cover,
+        ),
+        duration: const Duration(seconds: 4),
+        done: Done(
+          HomeScreen(),
+          animationDuration: const Duration(seconds: 0),
+        ),
+      ),
+    );
   }
 }
